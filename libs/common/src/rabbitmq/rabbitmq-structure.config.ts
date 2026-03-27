752469fb41ea93a53321336643cbcec6b333de40
Ali <a.arjmandi@cendas.net>
2026-03-27 08:50:52 +0100

    feat: :sparkles: update RabbitMQ configuration to define queues and routing keys for transaction and search services

--- libs/common/src/rabbitmq/rabbitmq-structure.config.ts
+++ libs/common/src/rabbitmq/rabbitmq-structure.config.ts
@@ -2,7 +2,25 @@ export const rabbitmqStructureConfig = {
   apps: {
     transactionService: {
       exchange: 'transaction.events',
-      queues: [],
+      queue: null,
+      publishRoutingKeys: [
+        'transaction.created',
+        'transaction.updated',
+        'transaction.deleted',
+      ],
+      subscribeRoutingKeys: ['search.index.updated', 'search.index.rejected'],
+    },
+    searchService: {
+      exchange: 'search.events',
+      queue: 'search-service.queue',
+      publishRoutingKeys: ['search.index.updated', 'search.index.rejected'],
+      subscribeRoutingKeys: [
+        'transaction.created',
+        'transaction.updated',
+        'transaction.deleted',
+        'ai.enriched',
+        'ai.rejected',
+      ],
     },
   },
 } as const;
