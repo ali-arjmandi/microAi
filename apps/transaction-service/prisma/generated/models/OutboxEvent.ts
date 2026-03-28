27088200c7d8a1738900321159d2974f46460d77
Ali <a.arjmandi@cendas.net>
2026-03-28 13:41:07 +0100

    refactor: :recycle: remove microAgent application and its related files, streamlining project structure and dependencies

--- apps/transaction-service/prisma/generated/models/OutboxEvent.ts
+++ apps/transaction-service/prisma/generated/models/OutboxEvent.ts
@@ -223,7 +223,7 @@ export type OutboxEventGroupByOutputType = {
   _max: OutboxEventMaxAggregateOutputType | null
 }
 
-type GetOutboxEventGroupByPayload<T extends OutboxEventGroupByArgs> = Prisma.PrismaPromise<
+export type GetOutboxEventGroupByPayload<T extends OutboxEventGroupByArgs> = Prisma.PrismaPromise<
   Array<
     Prisma.PickEnumerable<OutboxEventGroupByOutputType, T['by']> &
       {
